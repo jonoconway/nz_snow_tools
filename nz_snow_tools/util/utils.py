832ffc4613cd495548cea7ad9f8c2131aea1937d
conwayjp <jono.conway@niwa.co.nz>
2024-08-12 14:32:30 +1200

    bug fixes for interp

--- nz_snow_tools/util/utils.py
+++ nz_snow_tools/util/utils.py
@@ -648,7 +648,7 @@ def basemap_interp(datain, xin, yin, xout, yout, interpolation='NearestNeighbour
     # xout, yout = np.meshgrid(xout, yout)
     # ensure that coordiates are both 2D arrays
     assert xout.shape == yout.shape
-    assert xout.shape.ndim == 2
+    assert xout.ndim == 2
 
     # compute grid coordinates of output grid.
     xcoords = (len(xin) - 1) * (xout - xin[0]) / (xin[-1] - xin[0])
