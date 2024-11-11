a412a50814ee226fd10a27f09da7fa7825a224c0
Yinjing Lin <Yinjing.Lin@niwa.co.nz>
2024-11-11 13:55:27 +1300

    Add multithreading script

--- nz_snow_tools/hpc_runs/interp_met_nzcsm_multithread.py
+++ nz_snow_tools/hpc_runs/interp_met_nzcsm_multithread.py
@@ -21,8 +21,7 @@ import datetime
 import gc
 import pandas as pd
 import xarray as xr
-import dask
-from dask.distributed import Client, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from nz_snow_tools.util.utils import make_regular_timeseries, u_v_from_ws_wd, ws_wd_from_u_v
 from nz_snow_tools.met.interp_met_data_hourly_vcsn_data import interpolate_met, setup_nztm_dem, setup_nztm_grid_netcdf, trim_lat_lon_bounds
@@ -84,24 +83,10 @@ def process_output_orogrpahy(config, first_time, last_time, rot_pole_crs):
 def get_dataset_dict(config, first_time, last_time):
     dataset_dict = {}
     # for var in config['variables'].keys():
-    for var in ['air_temp']:
+    for var in ['air_temp', 'rh', 'solar_rad']:
         with xr.open_dataset(config['variables'][var]['input_file'], decode_times=True) as ds:
             ds = ds.rename_dims({config['variables'][var]['input_time_var']: 'time'}).rename_vars({config['variables'][var]['input_time_var']: 'time'})
             dataset_dict[var] = ds.sel(time=slice(first_time, last_time))
-    # dataset_dict['rh'][config['variables']['rh']['input_var_name']] = dataset_dict['rh'][config['variables']['rh']['input_var_name']] * 100  # convert to %
-    # dataset_dict['lw_rad'][config['variables']['lw_rad']['input_var_name']] = dataset_dict['lw_rad'][config['variables']['lw_rad']['input_var_name']] / (5.67e-8 * dataset_dict['air_temp'][config['variables']['air_temp']['input_var_name']] ** 4)
-
-    return dataset_dict
-
-def get_dataset_dict_one_timestep(config, time):
-    dataset_dict = {}
-    # for var in config['variables'].keys():
-    for var in ['air_temp']:
-        with xr.open_dataset(config['variables'][var]['input_file'], decode_times=True) as ds:
-            ds = ds.rename_dims({config['variables'][var]['input_time_var']: 'time'}).rename_vars({config['variables'][var]['input_time_var']: 'time'})
-            dataset_dict[var] = ds.sel(time=time)
-            # print(f"{datetime.datetime.now()}: {config['variables'][var]['input_var_name']} {dataset_dict[var][config['variables'][var]['input_var_name']].values[0,0]} at {time}")
-    # dataset_dict['rh'][config['variables']['rh']['input_var_name']] = dataset_dict['rh'][config['variables']['rh']['input_var_name']] * 100  # convert to %
     # dataset_dict['lw_rad'][config['variables']['lw_rad']['input_var_name']] = dataset_dict['lw_rad'][config['variables']['lw_rad']['input_var_name']] / (5.67e-8 * dataset_dict['air_temp'][config['variables']['air_temp']['input_var_name']] ** 4)
 
     return dataset_dict
@@ -112,18 +97,18 @@ def process_time_step(config, dataset_dict_vars, i_time, var, inp_lons, inp_lats
     for i_vars in dataset_dict_vars.keys():
         dataset_dict_timestep[i_vars] = dataset_dict_vars[i_vars].sel(time=i_time).load()
     
-    # if var == 'rh':
-    #     input_hourly = inp_nc_file[config['variables'][var]['input_var_name']].sel(time=i_time_step).values * 100  # convert to %
-    if var == 'wind_speed' or var == 'wind_direction':
-        pass
-    elif var == 'total_precip':
-        pass
-    else:
-        if config['variables'][var]['input_var_name'] in dataset_dict_timestep[var].data_vars:
-            input_hourly = dataset_dict_timestep[var][config['variables'][var]['input_var_name']].values
+    try:
+        if var == 'rh':
+            input_hourly = dataset_dict_timestep[var][config['variables'][var]['input_var_name']].values * 100  # convert to %
+        elif var == 'wind_speed' or var == 'wind_direction':
+            pass
+        elif var == 'total_precip':
+            pass
         else:
-            print(f"{i_time}: {config['variables'][var]['input_var_name']} not in {var} dataset")
-            return i_time, None
+            input_hourly = dataset_dict_timestep[var][config['variables'][var]['input_var_name']].values
+    except Exception as e:
+        print(f"{i_time}: {config['variables'][var]['input_var_name']} not in {var} dataset")
+        return i_time, None
     hi_res_out = interpolate_met(input_hourly, var, inp_lons, inp_lats, inp_elev_interp, out_rlons, out_rlats, elev, single_dt=True)
     hi_res_out[output_grid_dict['trimmed_mask'] == 0] = np.nan
     return i_time, hi_res_out
@@ -156,17 +141,29 @@ def process_input_orogrpahy(config):
 
     return intput_dict
 
-def interp_met_nzcsm_time(config_file):
+def process_input_orogrpahy_no_dem_file(config, var, inp_nc_file, intput_dict):
+    inp_lats = inp_nc_file[config['input_grid']['y_coord_name']].values
+    inp_lons = inp_nc_file[config['input_grid']['x_coord_name']].values
+    if 'rotated' in config['input_grid']['coord_system']:
+        rot_pole = inp_nc_file['rotated_pole']
+        assert intput_dict['rot_pole_crs'] == ccrs.RotatedPole(rot_pole.grid_north_pole_longitude, rot_pole.grid_north_pole_latitude, rot_pole.north_pole_grid_longitude)
+    else:
+        print('only set up for rotated pole coordinates')
+    if var in ['air_temp','air_pres']:
+        input_elev = inp_nc_file[config['input_grid']['dem_var_name']].values  # needed for pressure adjustment
+        inp_elev_interp = input_elev.copy() # needed for air temp
+    else:
+        inp_elev_interp = None
 
-    # 0, set up dask client
+    intput_dict['inp_lats'] = inp_lats
+    intput_dict['inp_lons'] = inp_lons
+    intput_dict['inp_elev_interp'] = inp_elev_interp
+
+
+def interp_met_nzcsm_multithread(config_file):
+
     n_procs = int(os.environ.get("SLURM_CPUS_PER_TASK", '6'))
-    print(f'    running with {n_procs} processes')
-    dask.config.set({
-        'distributed.worker.memory.target': 0.90,
-        'distributed.worker.memory.spill': 0.95,
-        'distributed.worker.memory.pause': 0.98,
-        'distributed.worker.memory.terminate': 0.99,
-    })
+    print(f'    running with {n_procs} threads')
 
     config = yaml.load(open(config_file), Loader=yaml.FullLoader)
 
@@ -174,7 +171,7 @@ def interp_met_nzcsm_time(config_file):
     last_time = parser.parse(config['output_file']['last_timestamp'])
     # Generate a time series of time
     time_series = pd.date_range(start=first_time, end=last_time, freq=f'{config["output_file"]["timestep"]}s')
-    
+
     # 1, process input orogrpahy
     intput_dict = process_input_orogrpahy(config)
 
@@ -187,7 +184,7 @@ def interp_met_nzcsm_time(config_file):
     # 4, run through each variable
 
     # for var in config['variables'].keys():
-    for var in ['air_temp']:#,'rh','solar_rad','lw_rad']:
+    for var in [ 'rh', 'air_temp','solar_rad']:#,'lw_rad''air_temp', ]:
         print(f"{datetime.datetime.now()}: processing {var}")
         # set up variable in output file
         t = out_nc_file.createVariable(config['variables'][var]['output_name'], 'f4', ('time', 'northing', 'easting',), zlib=True)  # ,chunksizes=(1, 100, 100)
@@ -202,125 +199,33 @@ def interp_met_nzcsm_time(config_file):
 
         # open input met including model orography (so we can use input on different grids (as long as they keep the same coordinate system)
         inp_nc_file = dataset_dict[var]
+
         if config['input_grid']['dem_file'] == 'none': # load coordinates of each file
-            inp_lats = inp_nc_file[config['input_grid']['y_coord_name']].values
-            inp_lons = inp_nc_file[config['input_grid']['x_coord_name']].values
-            if 'rotated' in config['input_grid']['coord_system']:
-                rot_pole = inp_nc_file['rotated_pole']
-                assert intput_dict['rot_pole_crs'] == ccrs.RotatedPole(rot_pole.grid_north_pole_longitude, rot_pole.grid_north_pole_latitude, rot_pole.north_pole_grid_longitude)
-            else:
-                print('only set up for rotated pole coordinates')
-            if var in ['air_temp','air_pres']:
-                input_elev = inp_nc_file[config['input_grid']['dem_var_name']].values  # needed for pressure adjustment
-                inp_elev_interp = input_elev.copy() # needed for air temp
-            else:
-                inp_elev_interp = None
-                   
-        with Client(n_workers=n_procs) as client:
-            tasks = []
+            process_input_orogrpahy_no_dem_file(config, var, inp_nc_file, intput_dict)
+
+        with ThreadPoolExecutor(max_workers=n_procs) as executor:
             dataset_dict_vars = {}
-            if var in ['air_temp']:#,'rh','solar_rad','lw_rad']:
+            if var in ['air_temp','rh','solar_rad']:#,'lw_rad']:
                 dataset_dict_vars[var] = dataset_dict[var]
-                tasks = [client.submit(process_time_step, config, dataset_dict_vars, i_time, var, inp_lons, inp_lats, inp_elev_interp, out_rlons, out_rlats, elev, output_grid_dict) for i_time in time_series]
-                # futures = client.map(lambda args: process_time_step(*args), tasks)
-                # futures = client.map(process_time_step, tasks)
-                for future in as_completed(tasks):
+                futures = [executor.submit(process_time_step, config, dataset_dict_vars, i_time, var, inp_lons, inp_lats, inp_elev_interp, out_rlons, out_rlats, elev, output_grid_dict) for i_time in time_series]
+                # process task results as they are available
+                for future in as_completed(futures):
                     try:
                         i_time, hi_res_out = future.result()
                         i_time_index = time_series.get_loc(i_time)
-                        # print(f"{datetime.datetime.now()}: put {hi_res_out[0,0]} at {i_time_index}:{i_time}")
                         if hi_res_out is not None:
                             t[i_time_index, :, :] = hi_res_out
-                        client.cancel(future)
-                        del hi_res_out  # Free memory immediately after use
-                        gc.collect()  # Explicitly call garbage collection
+                        # del hi_res_out  # Free memory immediately after use
+                        # gc.collect()  # Explicitly call garbage collection
                     except Exception as e:
                         print(f"Error processing time step {i_time}: {e}")
                         continue
             # # 'lw_rad'
             # air_temp = out_nc_file[config['variables']['air_temp']['output_name']][:, :, :]
             # out_nc_file[config['variables']['lw_rad']['output_name']][:, :, :] = out_nc_file[config['variables']['lw_rad']['output_name']][:, :, :] * (5.67e-8 * air_temp ** 4)
-
-        out_nc_file.close()
         print(f"{datetime.datetime.now()}: Done")
-
-def interp_met_nzcsm_squential(config_file):
-
-    config = yaml.load(open(config_file), Loader=yaml.FullLoader)
-    print('processing input orogrpahy') # load to get coordinate reference system for interpolation
-
-    # 1, read dem_file
-    if config['input_grid']['dem_file'] == 'none': # if no specific model orography file then open air pressure input variable file and extract coordinate system out of it
-        nc_file_orog = xr.open_dataset(config['variables']['air_pres']['input_file'])
-        if 'rotated' in config['input_grid']['coord_system']:
-            rot_pole = nc_file_orog['rotated_pole']
-            rot_pole_crs = ccrs.RotatedPole(rot_pole.grid_north_pole_longitude, rot_pole.grid_north_pole_latitude, rot_pole.north_pole_grid_longitude)
-        else:
-            print('currently only set up for rotated pole')
-    else: # config['input_grid']['dem_file'] != 'none': # assume all files have same coordinates and load coordiantes from separte file
-        nc_file_orog = xr.open_dataset(config['input_grid']['dem_file'])
-        if 'rotated' in config['input_grid']['coord_system']:
-            rot_pole = nc_file_orog['rotated_pole']
-            rot_pole_crs = ccrs.RotatedPole(rot_pole.grid_north_pole_longitude, rot_pole.grid_north_pole_latitude, rot_pole.north_pole_grid_longitude)
-        else:
-            print('currently only set up for rotated pole')
-            
-        input_elev = nc_file_orog[config['input_grid']['dem_var_name']].values # needed for pressure
-        inp_elev_interp = input_elev.copy()
-        # inp_elev_interp = np.ma.fix_invalid(input_elev).data
-        inp_lats = nc_file_orog[config['input_grid']['y_coord_name']].values
-        inp_lons = nc_file_orog[config['input_grid']['x_coord_name']].values
-
-    first_time = parser.parse(config['output_file']['first_timestamp'])
-    last_time = parser.parse(config['output_file']['last_timestamp'])
-
-    # 2, read the original data
-    dataset_dict = get_dataset_dict(config, first_time, last_time)
-
-    # 3, setup output
-    out_nc_file, output_grid_dict = process_output_orogrpahy(config, first_time, last_time, rot_pole_crs)
-
-    # 3, run through each variable
-    # for var in config['variables'].keys():
-    for var in ['air_temp']:
-        print('processing {}'.format(var))
-        # set up variable in output file
-        t = out_nc_file.createVariable(config['variables'][var]['output_name'], 'f4', ('time', 'northing', 'easting',), zlib=True)  # ,chunksizes=(1, 100, 100)
-        t.setncatts(config['variables'][var]['output_meta'])
-
-        # open input met including model orography (so we can use input on different grids (as long as they keep the same coordinate system)
-        inp_nc_file = dataset_dict[var]
-
-        if config['input_grid']['dem_file'] == 'none': # load coordinates of each file
-            inp_lats = inp_nc_file[config['input_grid']['y_coord_name']].values
-            inp_lons = inp_nc_file[config['input_grid']['x_coord_name']].values
-            if 'rotated' in config['input_grid']['coord_system']:
-                rot_pole = inp_nc_file['rotated_pole']
-                assert rot_pole_crs == ccrs.RotatedPole(rot_pole.grid_north_pole_longitude, rot_pole.grid_north_pole_latitude, rot_pole.north_pole_grid_longitude)
-            else:
-                print('only set up for rotated pole coordinates')
-            if var in ['air_temp','air_pres']:
-                input_elev = inp_nc_file[config['input_grid']['dem_var_name']].values  # needed for pressure adjustment
-                inp_elev_interp = input_elev.copy() # needed for air temp
-            else:
-                inp_elev_interp = None
-        
-        out_rlons = output_grid_dict['out_rlons']
-        out_rlats = output_grid_dict['out_rlats']
-        elev = output_grid_dict['elev']
-
-        # load variables relevant for interpolation
-        if var == 'air_temp':
-            inp_nc_var = inp_nc_file[config['variables'][var]['input_var_name']]
-            for i_time in range(inp_nc_var.sizes['time']):
-                input_hourly = inp_nc_var.isel(time=i_time).values
-                hi_res_out = interpolate_met(input_hourly, var, inp_lons, inp_lats, inp_elev_interp, out_rlons, out_rlats, elev, single_dt=True)
-                hi_res_out[output_grid_dict['trimmed_mask'] == 0] = np.nan
-                # save timestep to netCDF
-                t[i_time, :, :] = hi_res_out
-
     out_nc_file.close()
-
+        
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -330,4 +235,4 @@ if __name__ == '__main__':
         config_file = r'C:\Users\conwayjp\code\github\nz_snow_tools_jc\nz_snow_tools\hpc_runs\nzcsm_local.yaml'
         print('incorrect number of commandline inputs')
 
-    interp_met_nzcsm_time(config_file)
+    interp_met_nzcsm_multithread(config_file)
