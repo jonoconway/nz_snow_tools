7455b9a866b5c067879d213add4c2fc2515b50a5
conwayjp <jono.conway@niwa.co.nz>
2025-01-13 15:30:28 +1300

    bug fixes for ESRI crs definition

--- nz_snow_tools/hpc_runs/reassign_coords_fsm.py
+++ nz_snow_tools/hpc_runs/reassign_coords_fsm.py
@@ -27,6 +27,7 @@ da = xr.DataArray(
 dso.attrs["Conventions"] = "CF-1.8"
 dso.easting.attrs['standard_name'] = "projection_x_coordinate"
 dso.northing.attrs['standard_name'] = "projection_y_coordinate"
+dso.elevation.attrs['grid_mapping'] = "nztm"
 
 # Define the coordinate system using EPSG:2193
 # crs = xr.DataArray(np.array(0),
@@ -64,8 +65,7 @@ crs = xr.DataArray(np.array(0),
                                    'PARAMETER["Central_Meridian",173.0],'
                                    'PARAMETER["Scale_Factor",0.9996],'
                                    'PARAMETER["Latitude_Of_Origin",0.0],'
-                                   'UNIT["Meter",1.0],'
-                                   'AUTHORITY["EPSG", "2193"]]'),
+                                   'UNIT["Meter",1.0]]'),
                        "projected_crs_name": "NZGD2000 New Zealand Transverse Mercator",
                        "longitude_of_central_meridian": 173.0,
                        "latitude_of_projection_origin": 0.0,
@@ -79,14 +79,40 @@ crs = xr.DataArray(np.array(0),
                    }
                    )
 
+#from what I can tell ESRI needs the netCDF to have a minimal set of attrubutes in the crs definition, for all variables (except for lat/lon/easting/northing/time) to have the grid_mapping set
+
+
+crs_esri = xr.DataArray(np.array(0),
+                   attrs={
+                       "standard_name": 'crs',
+                       "grid_mapping_name": "transverse_mercator",
+                       "crs_wkt": ('PROJCS["NZGD_2000_New_Zealand_Transverse_Mercator",'
+                                   'GEOGCS["GCS_NZGD_2000",'
+                                   'DATUM["D_NZGD_2000",'
+                                   'SPHEROID["GRS_1980",6378137.0,298.257222101]],'
+                                   'PRIMEM["Greenwich",0.0],'
+                                   'UNIT["Degree",0.0174532925199433]],'
+                                   'PROJECTION["Transverse_Mercator"],'
+                                   'PARAMETER["False_Easting",1600000.0],'
+                                   'PARAMETER["False_Northing",10000000.0],'
+                                   'PARAMETER["Central_Meridian",173.0],'
+                                   'PARAMETER["Scale_Factor",0.9996],'
+                                   'PARAMETER["Latitude_Of_Origin",0.0],'
+                                   'UNIT["Meter",1.0]]'),
+                   }
+                   )
+
 
 first_time = da.time[0].dt.strftime('%Y%m%d%H%M').values
 last_time = da.time[-1].dt.strftime('%Y%m%d%H%M').values
 dso = dso.assign(snw=da, nztm=crs)
 dso.to_netcdf('swe_{}_{}.nc'.format(first_time, last_time))
 
+dso.assign(nztm=crs_esri).to_netcdf('swe_{}_{}_esri.nc'.format(first_time, last_time))
+
 dso['dswe'] = dso.snw * np.nan
 dso['dswe'][1:] = dso.snw.diff(dim='time', label='upper')  # label at end of the day - because the timestamp is in UTC it will return for the NZ day
 # queried e.g.  ds_full.sel('2022-07-18') will return the value for the 24-hour change to midnight '2022-07-19 00:00+12:00'
 dso['dswe'] = dso.dswe.assign_attrs(long_name='24-hour change in SWE', units='mm w.e.',
                                     description='change in snow pack storage in mm water equivalent for 24-hour preceeding timestamp', grid_mapping='nztm')
+
